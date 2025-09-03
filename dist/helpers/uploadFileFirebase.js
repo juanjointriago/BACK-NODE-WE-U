cc7c005b2d813c5ff89771990726fb3440dc22e1
Luis Lainez <luis.lainez@conauto.com.ec>
2025-09-03 10:55:43 -0500

    rebuild

--- dist/helpers/uploadFileFirebase.js
+++ dist/helpers/uploadFileFirebase.js
@@ -15,23 +15,13 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -42,12 +32,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.uploadFileFirebase = uploadFileFirebase;
+exports.uploadFileFirebase = void 0;
 const fs = __importStar(require("fs"));
 const firebase_1 = require("../config/firebase");
 function uploadFileFirebase(file, fileName, folder) {
+    var _a;
     return __awaiter(this, void 0, void 0, function* () {
-        var _a;
         try {
             const filePath = fs.realpathSync(file.tempFilePath);
             const extension = file.name.split('.').pop();
@@ -73,4 +63,5 @@ function uploadFileFirebase(file, fileName, folder) {
         }
     });
 }
+exports.uploadFileFirebase = uploadFileFirebase;
 //# sourceMappingURL=uploadFileFirebase.js.map
